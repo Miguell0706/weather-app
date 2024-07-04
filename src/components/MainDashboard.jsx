b944fbda675c42ceebe4fae115bcc5c868ae93ba
Miguel <Miguellozano3757@gmail.com>
2024-07-04 01:00:25 -0700

    working on base html for project

--- src/components/MainDashboard.jsx
+++ src/components/MainDashboard.jsx
@@ -2,9 +2,27 @@ import '../styles/MainDashboard.css'; // Adjusted import path
 
 function MainDashboard() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <section className='main-dashboard'>
+      <div className='main-header' >
+        <input type="text" placeholder="Search for a city" />
+        <h2>Tucson, Arizona</h2>
+      </div>
+      <div className='main-body'>
+        <div>
+          <div>
+            <p className='current-time'>12:00pm </p>
+            <p className='current-date'>June 22nd, 2023</p>
+            <p className='current-temperature'>32</p>
+          </div>
+          <div>
+            <p className='current-weather'>Sunny</p>
+            <p className='current-humidity'>Humidity</p>
+            <p className='current-wind'>Wind</p>
+            <p className='current-uv'>UV Index</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
